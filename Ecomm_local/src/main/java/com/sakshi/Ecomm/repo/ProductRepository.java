da5c47c46a4e9245ce732156a01411df5540570c
Sakshi Shelke <Sakshishelke316@gmail.com>
2025-12-06 22:53:27 +0530

    Removed Ecomm_local folder from repo

--- Ecomm_local/src/main/java/com/sakshi/Ecomm/repo/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sakshi.Ecomm.repo;
-
-import com.sakshi.Ecomm.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Long> {
-}
